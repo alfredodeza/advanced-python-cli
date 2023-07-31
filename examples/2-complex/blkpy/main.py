0e5a125e08de97894bfbfc05faef6cab0e511d15
Alfredo Deza <alfredo@deza.pe>
2023-07-30 20:56:02 -0400

    add more examples

--- examples/2-complex/blkpy/main.py
+++ examples/2-complex/blkpy/main.py
@@ -1,10 +1,33 @@
 import click
 from blkpy.util import run_lsblk
 
-@click.command()
+
+# 1. Make the verbose flag global using context
+# 2. Add flags with types associated with them
+# 3. Use the special context object to pass data between commands
+
+@click.group()
 @click.option('--verbose', '-v', is_flag=True)
-@click.argument('device')
-def main(device, verbose):
-    print(f"Device: {device}")
+@click.option('--debug-level', '-d', type=float, default=1)
+@click.pass_context
+def main(ctx, verbose, debug_level):
+    ctx.ensure_object(dict)
+    ctx.obj.update(locals())
+    if type(debug_level) == int:
+        print("Debug level is 100")
+    else:
+        print(f"the type of debug level is {type(debug_level)}")
     print(f"Verbose: {verbose}")
+    pass
+
+
+@main.command()
+@click.argument('device')
+@click.pass_context
+def info(ctx, device):
+    verbose = ctx.obj['verbose']
+    if verbose:
+        print(f"Device: {device}")
+        print(f"{str(ctx.obj)}")
     print(f"{run_lsblk(device)}")
+    
--- examples/3-envvars/blkpy/main.py
+++ examples/3-envvars/blkpy/main.py
@@ -1,10 +1,17 @@
 import click
 from blkpy.util import run_lsblk
 
+# 1. Allow the verbose flag to be an environment variable
+# 2. Add a new option that allows setting the level of verbosity, 
+# from 1 to 4 with a default
+
+
 @click.command()
 @click.option('--verbose', '-v', is_flag=True)
+@click.option('--debug-level', '-d', envvar='BLKPY_DEBUG_LEVEL', type=int, default=1, help='Set the debug optionally with an environment variable')
 @click.argument('device')
-def main(device, verbose):
+def main(device, verbose, debug_level):
     print(f"Device: {device}")
     print(f"Verbose: {verbose}")
-    print(f"{run_lsblk(device)}")
+    print(f"Debug level: {debug_level}")
+    #print(f"{run_lsblk(device)}")
